e699cab51875e2fec5ff2cb996bd130f533aec75
mariacandland <MariaCandland@Marias-MacBook-Pro.local>
2015-10-08 19:11:19 -0600

    html files to php

---
+++ about.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Adding the Page Title from the index, about, or contact pages -->
+    <title>About | Maria-Candland</title>
+
+    <!-- Tell the browser to render the view width correctly -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Link our stylesheet -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+
+  <body id="about">
+    <header>
+      <div class="row clearfix">
+        <a href="about.html"><img class="logo" src="images/logo.png"/></a>
+
+        <!-- Our Site Navigation -->
+        <nav>
+        	<a href="index.html">Home</a>
+        	<a href="about.html">About</a>
+        	<a href="contact.html">Contact</a>
+        </nav>
+
+      </div>
+    </header>
+
+    <!-- Font -->
+          <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+      <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+
+    <div class="row clearfix">
+
+      <!-- Content area that is a two third column -->
+      <div class="column one-half">
+        <h3>Column Heading</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+
+      <!-- Sidebar Area & Widgets-->
+      <div class="column one-half sidebar">
+
+      	<!-- Begin widget -->
+      	<div class="widget">
+      		<h3>Widget Title</h3>
+      		<p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>
+      	</div>
+      	<!-- End Widget -->
+
+
+      	<!-- Begin widget -->
+      	<div class="widget">
+      		<h3>Widget Title</h3>
+      		<p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>
+      	</div>
+      	<!-- End Widget -->
+
+
+      	<!-- Begin widget -->
+      	<div class="widget">
+      		<h3>Widget Title</h3>
+      		<p>Haircut engaged something Dibble Dibble Dibble Dibble nonsensical alone sadly prickle-ly playthings Wickersham Brothers guaranteed murmur.</p>
+      	</div>
+      	<!-- End Widget -->
+
+      </div>
+    </div>
+
+    <footer>
+      <!-- Our Site Navigation -->
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+
+      <!-- Site Copyright -->
+    	<p>Copyright 2015 - Maria Candland</p>
+    </footer>
+
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <!-- Adding the Page Title from the index, about, or contact pages -->
+    <title>Home | Maria-Candland</title>
+
+    <!-- Tell the browser to render the view width correctly -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Link our stylesheet -->
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body id="home">
+    <header>
+      <div class="row clearfix">
+          
+          <!-- Font -->
+          <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
+          <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
+
+        <!-- Our Logo -->
+        <a href="index.html"><img class="logo" src="images/logo.png"/></a>
+
+        <!-- Our Site Navigation -->
+        <nav>
+        	<a href="index.html">Home</a>
+        	<a href="about.html">About</a>
+        	<a href="contact.html">Contact</a>
+        </nav>
+
+      </div>
+    </header>
+
+
+    <!-- Our site's hero area -->
+    <div class="hero">
+      <div class="row clearfix">
+          <h1>Hello, nice to see you again!</h1>
+      </div>
+    </div>
+
+
+
+    <!-- A row of two 1/2 width columns -->
+    <div class="row clearfix">
+      <div class="column one-half">
+        <h3>Column Heading</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+
+      <div class="column one-half">
+        <h3>Column Heading</h3>
+        <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+      </div>
+    </div>
+
+
+    <footer>
+      <!-- Our Site Navigation -->
+      <nav>
+        <a href="index.html">Home</a>
+        <a href="about.html">About</a>
+        <a href="contact.html">Contact</a>
+      </nav>
+
+      <!-- Site Copyright -->
+    	<p>Copyright 2015 - Maria Candland</p>
+    </footer>
+
+  </body>
+</html>
