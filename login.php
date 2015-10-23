8375c1ff563e71d957d8993ae8d6d6eadd55bc59
mariacandland <MariaCandland@Marias-MacBook-Pro.local>
2015-10-22 18:03:40 -0600

    login-sass

--- login.php
+++ login.php
@@ -16,20 +16,39 @@
           <link href='http://fonts.googleapis.com/css?family=Bree+Serif' rel='stylesheet' type='text/css'>
       <link href='http://fonts.googleapis.com/css?family=Indie+Flower' rel='stylesheet' type='text/css'>
 
-    <div class="row">
+    
+   <div id="login">
 
-      <!-- Half column with heading and message -->
-      <div class="column one-half">
-        <h3>Column Heading</h3>
-        <p>Aenean lacinia bibendum nulla sed consectetur. Donec ullamcorper nulla non metus auctor fringilla. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-      </div>
+  <form method="post" name="LoginForm" id="LoginForm">
 
-      <!-- Half column with google map -->
-      <div class="column one-half">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m23!1m12!1m3!1d98845.8748494732!2d-104.97167958467622!3d39.267073399017!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m8!3e6!4m0!4m5!1s0x876ca386de31fa7f%3A0xc26ce1a94f1b9c32!2s650+Sky+View+Ln%2C+Larkspur%2C+CO+80118!3m2!1d39.267096699999996!2d-104.89786269999999!5e0!3m2!1sen!2sus!4v1444235282804" width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
-      </div>
 
-    </div>
+    <p>function LoginPage()</p>
+    <p>{</p><br />
+    <p><span></span>//At this point the user have to put his login details</p><br />
+    <p><span></span>echo = '&#60;form action="/" method="post"&#62;';</p>
+    <p><span></span>echo .=	'&#60;input type="text" name="<strong>username</strong>" value="<input type="text" name="username">" /&#62;';</p>
+    <p><span></span>echo .= '&#60;input type="text" name="<strong>password</strong>" value="<input type="password" name="password" />" /&#62;';</p>
+    <p><span></span>echo .= '&#60;input type="submit" value="<input type="submit" name="submit" value="submit request" />" /&#62;';</p>
+    <p><span></span>echo .= '&#60;/form&#62;';</p><br />
+    <p><span></span>//after that, check if all is right</p><br />
+    <p>}</p>
+
+    <p>if(isset($_POST['username']) && isset($_POST['passowrd']))</p>
+    <p>{</p><br />
+
+    <p><span></span>if(!ok($_POST['username'],$_POST['password'])){</p>
+    <p><span></span><span></span>echo 'denied!';</p>
+    <p><span></span><span></span>DIE();</p>
+    <p><span></span>}</p><br />
+
+    <p>}</p>
+
+  </form>
+
+</div>
+    
+
+  
 
    
   <!-- Include the footer -->
